6aea594e92ec478107080039709de1de61ae766b
vivekkantariya <kantariyavivek75@gmail.com>
2025-02-04 19:46:21 +0530

    still

--- Grocerry_Management/Grocerry_Management/wsgi.py
+++ Grocerry_Management/Grocerry_Management/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for Grocerry_Management project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Ensure this points to the correct settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Grocerry_Management.settings')
 
 application = get_wsgi_application()
