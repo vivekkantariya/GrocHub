3ed75e055bceea9bb410cbdf31167462c66fe353
vivekkantariya <kantariyavivek75@gmail.com>
2025-02-04 19:53:58 +0530

    gunicorn

--- Grocerry_Management/Grocerry_Management/wsgi.py
+++ Grocerry_Management/Grocerry_Management/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Ensure this points to the correct settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Grocerry_Management.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.Grocerry_Management.settings')
 
 application = get_wsgi_application()
